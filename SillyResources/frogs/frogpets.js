58bbbd7eb4045652a19db8141c28de47a7fc8523
Viper <93675303+ViperKK@users.noreply.github.com>
2023-10-20 22:38:30 -0600

    thirdpartyable

--- SillyResources/frogs/frogpets.js
+++ SillyResources/frogs/frogpets.js
@@ -76,8 +76,8 @@ var arr_names = ["Abs", "Agnus", "Al-gea", "Amphibulus", "Scrubby", "Crusty", "B
 var arr_states = ["feed", "pet", "hug", "comfort"];
 
 //strings
-var str_path_img = "/SillyResources/frogs/IMG";
-var str_path_snd = "/SillyResources/frogs/SND";
+var str_path_img = "https://viperkk.github.io/SillyResources/frogs/IMG";
+var str_path_snd = "https://viperkk.github.io/SillyResources/frogs/SND";
 //sounds
 var snd_oliver;
 var snd_princess;
